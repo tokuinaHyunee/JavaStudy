677c3d2fc12c5ea4318b96908f5949ad1eca0629
SH <sanghyen199920@gmail.com>
2026-01-17 00:12:23 +0900

    This is Java Study

---
+++ src/main/java/com/javastudy/Chapter06/Calculator.java
@@ -0,0 +1,13 @@
+package com.javastudy.Chapter06;
+
+public class Calculator {
+    //정사각형의 넓이
+    double areaRectangle(double width) {
+        return width * width;
+    }
+
+    //직사각형의 넓이
+    double areaRectangle(double width, double height) {
+        return width * height;
+    }
+}
